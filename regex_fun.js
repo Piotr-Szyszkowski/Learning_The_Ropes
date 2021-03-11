5224f8530e758917431a52de63cb7d17390cde9a
Piotr-Szyszkowski <79057797+Piotr-Szyszkowski@users.noreply.github.com>
2021-03-11 18:08:23 +0000

    .

---
+++ regex_fun.js
@@ -0,0 +1,7 @@
+//function rex(str) {
+let regEx = /bat/
+}//
+
+let t1 = "I am Batman, the Caped Crusader of Gotham. Fatman once appeared in comics too playing poker. Joker is my nemesis. Is Joker my nemesis. String continues, spring has come and sprung is the trap.";
+let myEx = / is|is /ig;
+console.log(t1.match(myEx));
