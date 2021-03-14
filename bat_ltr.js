6224d7450f3b9962a1e6bf477d0f4b8e7470a16c
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-03-14 20:49:19 +0000

    working on kata etc..

--- bat_ltr.js
+++ bat_ltr.js
@@ -1,13 +1,19 @@
-function isPalindrome(line) {
-   let lineStr = line.toString(10); 
-   let rev = line.toString(10).split("").reverse().join("");
-   return rev === lineStr;
-  };
 
-  let t1 = 123213;
-  let t2 = "banab";
-  /*console.log(t1.toString(10));
-  console.log(t1);
-  console.log(t2);  
-  console.log(t2.toString(10));*/
-  console.log(isPalindrome(t1));
+// below saved in separate file
+function domainName(url){
+   let myEx = /(?<!\/\.)(\w+)(?=\.)/g;
+   let ex1 = url.match(myEx);
+   for (let i = 0; i < ex1.length; i++) {
+      if (ex1[i] !== "www") {
+         return ex1[i];
+      }
+   }
+ };
+
+
+let t1 = "http://google.com";// "google"
+let t2 = "http://google.co.jp";// "google"
+let t3 = "www.xakep.ru";//"xakep"
+let t4 = "https://youtube.com";//"youtube"
+let t5 = "https://www.y0f5p6z9outvnw7khzw5.io/index.php"; // "y0f5p6z9outvnw7khzw5"
+console.log(domainName(t5));
---
+++ kata_extract_the_domain_name_from_a_URL.js
@@ -0,0 +1,22 @@
+/*
+************** Extract the domain name from a URL ****************
+.. work in progress ..
+*/
+
+function domainName(url){
+    let myEx = /(?<!\/\.)(\w+)(?=\.)/g;
+    let ex1 = url.match(myEx);
+    for (let i = 0; i < ex1.length; i++) {
+       if (ex1[i] !== "www") {
+          return ex1[i];
+       }
+    }
+  };
+ 
+ 
+ let t1 = "http://google.com";// "google"
+ let t2 = "http://google.co.jp";// "google"
+ let t3 = "www.xakep.ru";//"xakep"
+ let t4 = "https://youtube.com";//"youtube"
+ let t5 = "https://www.y0f5p6z9outvnw7khzw5.io/index.php"; // "y0f5p6z9outvnw7khzw5"
+ console.log(domainName(t5));
